1bcfcb95c64eb20426f99344e2ef7e735e633b12
Petrik Diak <diak@petrik.hu>
2025-02-07 12:20:23 +0100

    Film osztály kész, 2 konstruktorral, 3 metódussal

---
+++ OOPGyakorlas/Film.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPGyakorlas
+{
+	internal class Film
+	{
+		private string cim;
+		private string rendezo;
+		private int hosszPercekben;
+		private string mufaj;
+		private bool megjelent;
+
+		public Film(string cim, string rendezo)
+		{
+			this.cim = cim;
+			this.rendezo = rendezo;
+			hosszPercekben = 120;
+			mufaj = "Ismeretlen";
+			megjelent = false;
+		}
+
+		public Film(string cim, string rendezo, int hosszPercekben, string mufaj, bool megjelent)
+		{
+			this.cim = cim;
+			this.rendezo = rendezo;
+			this.hosszPercekben = hosszPercekben;
+			this.mufaj = mufaj;
+			this.megjelent = megjelent;
+		}
+
+		public string Cim { get => cim; set => cim = value; }
+		public string Rendezo { get => rendezo; set => rendezo = value; }
+		public int HosszPercekben { get => hosszPercekben; set => hosszPercekben = value; }
+		public string Mufaj { get => mufaj; set => mufaj = value; }
+		public bool Megjelent { get => megjelent; set => megjelent = value; }
+
+
+		public void HosszNovelese(int percek)
+		{
+			hosszPercekben += percek;
+		}
+
+		public string Jatszas()
+		{
+			return $"{cim} elindult";
+		}
+
+		public void MufajModositas(string ujMufaj)
+		{
+			mufaj = ujMufaj;
+		}
+		public override string? ToString()
+		{
+			return $"{cim} - {rendezo} / {hosszPercekben} / {mufaj} / {megjelent}";
+		}
+	}
+}
--- OOPGyakorlas/Program.cs
+++ OOPGyakorlas/Program.cs
@@ -13,6 +13,16 @@
 			konyv1.Arnoveles(1000);
 
 			Console.WriteLine("\n" + konyv1.ToString() + "\n" + konyv1.HanyOldalMaradt(20));
-		}
+
+			Film film1 = new("Tűzgyűrű", "Guillermo del Toro", 132, "sci-fi", true);
+			Film film2 = new("Készülő film", "Én");
+            
+			Console.WriteLine(film1.ToString() + "\n" + film2.ToString());
+
+			film1.MufajModositas("RomCom");
+			film1.HosszNovelese(25);
+
+            Console.WriteLine(film1.ToString() + "\n" + film1.Jatszas());
+        }	
 	}
 }
